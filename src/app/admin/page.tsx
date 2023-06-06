5136bad52cce41e033db79c8cf1a711741dca0b3
schrodd <andres_alejandro_rodriguez@hotmail.com>
2023-06-06 04:11:42 -0300

    fixed cors

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,25 +1,32 @@
 "use client"
 import { headers } from "next/dist/client/components/headers"
 import Image from "next/image"
-
-async function getUser(data: any){
-  const {username: user, password} = data
-  await fetch('https://food-order-app-production-84a2.up.railway.app/login', {
-    method: 'POST',
-    body: JSON.stringify({
-      user,
-      password
-    })
-  })
-  .then((res) => {
-    console.log(res)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-}
+import { useAppDispatch, useAppSelector } from '../../redux/hooks'
+import { set } from '../../redux/sessionSlice'
 
 export default function Page(){
+  const dispatch = useAppDispatch()
+  const authKey = useAppSelector((state) => state.sessionSlice.authKey)
+  async function getUser(data: any){
+    const {username, password} = data
+    console.log({username, password})
+    await fetch('http://localhost:3000/login', {
+      headers: new Headers({'content-type': 'application/json'}),
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      dispatch(set(data.access_token))
+      console.log('auth token saved')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>){
     event.preventDefault()
     const data = Object.fromEntries(new FormData(event.currentTarget) as Iterable<[PropertyKey, string]>)
@@ -35,6 +42,9 @@ export default function Page(){
           <input type="password" name="password" placeholder="Contraseña" className={inputTw}/>
           <button id="admin-login" type="submit" className="bg-gradient-to-tr from-red-500 to-rose-500 border text-slate-100 py-2 px-6 rounded-full font-medium">Iniciar sesión</button>
         </form>
+        <div className="w-1/2">
+          <p className="break-all text-xs">Active token: {authKey}</p>
+        </div>
       </div>
     </main>
   )
